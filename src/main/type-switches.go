e45ae006c00c726f5869e958eeea04cc77b954d6
Volkan Özçelik <volkan@o2js.com>
2016-07-07 22:19:20 -0700

    stuff.

---
+++ src/main/type-switches.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v.\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long.\n", v, len(v))
+	default:
+		fmt.Printf("I don’t know about type %T.\n", v)
+	}
+}
+func main() {
+	do(21)
+	do("Geronimo!")
+	do(true)
+}
