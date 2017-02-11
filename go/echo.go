1a67131c5fe04bf7f560493c6c9c1279b402f423
Volkan Özçelik <volkan.ozcelik@gmail.com>
2017-02-11 22:27:30 +0000

    recent changes

---
+++ go/echo.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func echo1() {
+	var s, sep string
+
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+
+	fmt.Println(s)
+}
+
+func echo2() {
+	var s, sep = "", ""
+
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+
+	fmt.Println(s)
+}
+
+func echo3() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
+
+func main() {
+	echo1()
+	echo2()
+	echo3()
+}
---
+++ go/lissajous.go
@@ -0,0 +1,57 @@
+package main
+
+// go run lissajous.go > anim.gif
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles = 5
+		res    = 0.0001
+		size   = 100
+		frames = 1024
+		delay  = 2
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: frames}
+	phase := 0.0
+
+	for i := 0; i < frames; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+
+		phase += 0.1
+
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+
+	gif.EncodeAll(out, &anim)
+}
