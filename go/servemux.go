40f9785e473890f9af19b267bfaebd4a4b0da227
Volkan Özçelik <volkan.ozcelik@gmail.com>
2017-02-14 19:23:07 +0000

    minor changes

--- go/servemux.go
+++ go/servemux.go
@@ -12,9 +12,16 @@ func main() {
     mux := http.NewServeMux()
 
     mux.Handle("/list", http.HandlerFunc(db.list))
-    mux.Handle("/price", http.HandlerFunc(db.price))
+    //mux.Handle("/price", http.HandlerFunc(db.price))
+    mux.HandleFunc("/price", db.price)
 
     log.Fatal(http.ListenAndServe("0.0.0.0:8080", mux))
+
+    // Using DefaultServeMux
+    // db := database{" shoes": 50, "socks": 5}
+    // http.HandleFunc("/ list", db.list)
+    // http.HandleFunc("/ price", db.price)
+    // log.Fatal( http.ListenAndServe(" localhost: 8000", nil))
 }
 
 type dollars float32
