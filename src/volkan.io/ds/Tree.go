4da23b789c153883144c42a673904c2b6b29c8c5
Volkan Özçelik <volkan.ozcelik@gmail.com>
2017-03-22 05:07:50 +0000

    added a toString implementation; not verified if it works

--- src/volkan.io/ds/Tree.go
+++ src/volkan.io/ds/Tree.go
@@ -2,7 +2,42 @@ package ds
 
 // Tree is a binary tree.
 type BTree struct {
-	Left  *Tree
+	Left  *BTree
 	Value interface{}
-	Right *Tree
+	Right *BTree
+}
+
+
+func prettyFormat(node BTree*, indent string, last bool) string {
+	result := indent
+
+	if last {
+		result += "\\-"
+		indent += "| "
+	} else {
+		result += "|-"
+		indent += "| "
+	}
+
+	result += (*node).Value + "\n"
+
+	children := make([]*BTree, 0)
+
+	if node.Left != nil {
+		children = append(children, node.Left)
+	}
+
+	if node.Right != nil {
+		children = append(children, node.Right)
+	}
+
+	for i := 0; i < len(children); i++ {
+		result += prettyFormat(children[i], indent, i == len(children) - 1)
+	}
+
+	return result
+}
+
+func (t *BTree) String() string {
+	return prettyFormat(t, "", false)
 }
